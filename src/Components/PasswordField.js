809675f2316722cf2a6042ad38fa123a5e107923
zad sasad <zad@sasad.com>
2019-07-18 01:18:35 +0800

    Add hasha library and use it for auth.

--- src/Components/PasswordField.js
+++ src/Components/PasswordField.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import hasha from 'hasha';
+import { Redirect } from 'react-router-dom';
 
 class PasswordField extends React.Component {
     constructor(props) {
@@ -14,7 +16,12 @@ class PasswordField extends React.Component {
     }
     
     handleSubmit(event) {
-        alert('A name was submitted: ' + this.state.value);
+        
+        if(hasha(this.state.value) === "75da598c06f0d35b9f7dd8d7bad7d31d16e29cf2ead925beac365404d09aa0dcf2411d3ea2d93e4f760ab685b9f65c93989189974217af885fb7bab9d9dc47fb")
+            return <Redirect to='/na/' />
+        else
+            alert('pass failed');
+
         event.preventDefault();
     }
 
